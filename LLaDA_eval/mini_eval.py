cb237b75518289435dd43b125079c9ece44ba6e0
zeynebnk <70820835+zeynebnk@users.noreply.github.com>
2025-03-01 10:22:37 -0800

    Update mini_eval.py

--- LLaDA_eval/mini_eval.py
+++ LLaDA_eval/mini_eval.py
@@ -1,6 +1,5 @@
-## modify these paths accordingly and run -> ~5-7 mins
-bench_data_path = "gsmSUBSAMPLE.txt"
-output_dir =  "outputs/"
+bench_data_path = "supermini.txt" 
+output_dir =  "/outputs/"
 remasking_method = "low_confidence"
 
 import subprocess
